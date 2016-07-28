da453dea9098d4d7a25374f62f9ebea864101c09
SSApps <scott.spencer.88@csun.edu>
2016-07-27 20:46:14 -0700

    Initial Commit

---
+++ SevenSegment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SevenSegment
+//
+//  Created by MegamanX on 7/27/16.
+//  Copyright © 2016 ScottSpencer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SevenSegment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SevenSegment
+//
+//  Created by MegamanX on 7/27/16.
+//  Copyright © 2016 ScottSpencer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
